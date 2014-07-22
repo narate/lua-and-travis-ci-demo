8dc2a2174e853e6999eb90848b4973c3879d685d
Narate Ketram <koonnarate@gmail.com>
2014-07-22 16:01:59 +0700

    - Add .travis.yml - Add spec/hello_spec.lua

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-lua-and-travis-ci-demo
+Lua, Busted, travis-ci
 ======================
 
 Lua, Busted, travis-ci
---
+++ spec/hello_spec.lua
@@ -0,0 +1,6 @@
+describe('Test hello', function()
+    it('should return 5', function()
+        local str = 'hello'
+        assert.equals(5, #str)
+    end)
+end)
