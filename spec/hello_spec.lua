9795b5de7358e24cd714e11ff65dc57f5f08af53
Narate Ketram <koonnarate@gmail.com>
2014-07-22 16:06:26 +0700

    - Add hello, world! char count

--- spec/hello_spec.lua
+++ spec/hello_spec.lua
@@ -3,4 +3,8 @@ describe('Test hello', function()
         local str = 'hello'
         assert.equals(5, #str)
     end)
+    it('should return 13', function()
+        local str = 'hello, world!'
+        assert.equals(13, #str)
+    end)
 end)
